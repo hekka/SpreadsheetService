540c2449b5063ca1e4901e352299be3f4d8184ec
unknown <henri.siik@R9YX4F.bouvet.no>
2014-03-20 14:42:13 +0100

    initial comm

---
+++ EconomySheetUpdater/Models/AppFlowMetadata.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Mvc;
+
+namespace Google.Apis.Sample.MVC4
+{
+    public class AppFlowMetadata : FlowMetadata
+    {
+        private static readonly IAuthorizationCodeFlow flow =
+            new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+                {
+                    ClientSecrets = new ClientSecrets
+                        {
+                            ClientId = "711463205843-84adg69j7ln2q9lmnkv5302ph7gpsmmq.apps.googleusercontent.com",
+                            ClientSecret = "vX5g25ZfClkWWsNDXETsRl8H"
+                        },
+                    Scopes = new[] { "https://spreadsheets.google.com/feeds", "https://docs.google.com/feeds" },
+                    
+                });
+
+        public override string GetUserId(Controller controller)
+        {
+            // In this sample we use the session to store the user identifiers.
+            // That's not the best practice, because you should have a logic to identify
+            // a user. You might want to use "OpenID Connect".
+            // You can read more about the protocol in the following link:
+            // https://developers.google.com/accounts/docs/OAuth2Login.
+
+            return "kirkegata37";
+        }
+
+        public string Redirect {
+            get { return "urn:ietf:wg:oauth:2.0:oob"; }
+        }
+
+        public override IAuthorizationCodeFlow Flow
+        {
+            get { return flow; }
+        }
+    }
+}
