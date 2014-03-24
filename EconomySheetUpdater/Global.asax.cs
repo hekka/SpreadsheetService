ef8aaca60772de0fe9139fd356cad1e99549e913
hekka <hekkaa@gmail.com>
2014-03-24 10:07:42 +0100

    removed dependencies

--- EconomySheetUpdater/Global.asax.cs
+++ EconomySheetUpdater/Global.asax.cs
@@ -23,7 +23,7 @@ namespace EconomySheetUpdater
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             BootstrapSupport.BootstrapBundleConfig.RegisterBundles(System.Web.Optimization.BundleTable.Bundles);
-            BootstrapMvcSample.ExampleLayoutsRouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BootstrapMvcSample.ExampleLayoutsRouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
