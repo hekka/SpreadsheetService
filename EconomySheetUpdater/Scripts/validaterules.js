42e96813d20270b50598d23c422947ee25b0f5f7
hekka <hekkaa@gmail.com>
2014-04-23 15:33:55 +0200

    fixed javascript loading

---
+++ EconomySheetUpdater/Scripts/validaterules.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $("#submitButton").click(function () {
+        $("#insertForm").validate({
+            rules: {
+                required: true
+            },
+            messages: {
+                required: 'Please enter a value'
+            }
+        });
+    });
+});
